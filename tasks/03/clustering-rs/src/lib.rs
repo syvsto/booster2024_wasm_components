3a255dc8ee84d70bcaff26b3bb4c164356208989
Amar Topalovic <amar.topalovic@protonmail.com>
2024-02-17 12:35:49 +0100

    fix: restore wasm module functionality

--- tasks/03/clustering-rs/src/lib.rs
+++ tasks/03/clustering-rs/src/lib.rs
@@ -3,6 +3,7 @@ use linfa::traits::{Fit, Predict};
 use linfa::DatasetBase;
 use linfa_clustering::KMeans;
 use ndarray;
+use wasm_bindgen::prelude::wasm_bindgen;
 use wit_bindgen;
 
 wit_bindgen::generate!({
@@ -35,22 +36,9 @@ impl Guest for Clustering {
     }
 }
 
-/*
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn it_works() {
-        let data_x = vec![-1., 1.0, 1.0, 15.3];
-        let data_y = vec![-3., 1.1, 1.4, 16.2];
-        let data = data_x
-            .iter()
-            .zip(data_y.iter())
-            .map(|(x, y)| vec![*x, *y])
-            .collect();
-        let res: Vec<u32> = Clustering::run();
-        println!("{:?}", res);
-    }
+#[wasm_bindgen]
+pub fn cluster(points_x: Vec<f64>, points_y: Vec<f64>, n_clusters: u32) -> Vec<u32> {
+    let points: Vec<Vec<f64>> = points_x.iter().zip(points_y.iter()).map(|(x, y)| vec![*x, *y]).collect();
+    Clustering::run(points, n_clusters)
 }
-*/
+
